aa6f0d6397b1bd9f92683123229c88a11a481723
Sam Rosenthal <39362114+SammRosenthal@users.noreply.github.com>
2023-07-19 11:54:04 -0400

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,12 @@ vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>?', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>FW', builtin.grep_string, {})
+
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = { ["<C-q>"] = actions.send_to_qflist },
+			n = { ["<C-q>"] = actions.send_to_qflist },
+		},
+	},
+})
